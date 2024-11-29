5953f618822ff9ed192e53346e7b84522a0673b8
Andrei Belskii <mrandrewer@yandex.ru>
2024-11-29 22:16:44 +0300

    Create field grid layout

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QGridLayout,
+    QPushButton,
+    QWidget,
+    QSizePolicy
+)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.__init_ui()
+
+
+    def __init_ui(self):
+        field_layout = self.__init_field()
+        center_widget = QWidget(parent=self)
+        center_widget.setLayout(field_layout)
+        self.setCentralWidget(center_widget)
+
+    
+    def __init_field(self):
+        layout = QGridLayout()
+        for i in range(0, 3):
+            for j in range(0, 3):
+                btn = QPushButton()
+                btn.setText(f"{i} {j}")
+                btn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+                layout.addWidget(btn, i, j)
+        return layout       
 
 
 if __name__ == '__main__':
